4dceee92b0edcf47359c472f68fbdd1738e2327b
root <root@localhost>
2025-06-01 18:25:37 +0000

    WIP

--- src/test/java/com/example/bank/web/AccountControllerTest.java
+++ src/test/java/com/example/bank/web/AccountControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest
 @WebMvcTest(AccountController.class)
 public class AccountControllerTest {
 
