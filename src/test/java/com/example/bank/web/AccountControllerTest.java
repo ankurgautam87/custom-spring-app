7f8d239d6ae340d052c7aeae771588f9044cf35f
root <root@localhost>
2025-06-01 18:34:55 +0000

    WIP

--- src/test/java/com/example/bank/web/AccountControllerTest.java
+++ src/test/java/com/example/bank/web/AccountControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -91,6 +92,7 @@ public class AccountControllerTest {
     }
 
     @Configuration
+    @EnableWebMvc
     static class TestConfig {
 
         @Bean
