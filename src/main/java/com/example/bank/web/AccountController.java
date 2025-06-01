b2c7bc50a421f9beb1c32f901a32871047078b67
root <root@localhost>
2025-06-01 18:43:36 +0000

    WIP

--- src/main/java/com/example/bank/web/AccountController.java
+++ src/main/java/com/example/bank/web/AccountController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import com.example.bank.domain.BankAccount;
@@ -25,6 +26,7 @@ public class AccountController {
     }
 
     @GetMapping
+    @ResponseBody
     public AccountForm showForm() {
         return new AccountForm();
     }
