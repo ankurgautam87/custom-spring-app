c37f90c85295a755969359dc2d278e21754663e6
root <root@localhost>
2025-06-01 17:34:18 +0000

    WIP

--- src/main/java/com/example/bank/web/AccountController.java
+++ src/main/java/com/example/bank/web/AccountController.java
@@ -1,6 +1,6 @@
 package com.example.bank.web;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
